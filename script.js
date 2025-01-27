e351408b113540b5e7528d24ceceb083730055c8
unknown <adriana.maslii@infis.cz>
2025-01-27 08:40:31 +0100

    message

---
+++ script.js
@@ -0,0 +1,39 @@
+// const cislo = Number(prompt("zadejte cislo: "))
+// if (cislo==5){
+//     alert("Zadali jste 5")
+// }else if(cislo==10){
+//     alert("Zadali jste 10")
+// }else if(cislo==12){
+//     alert("Zadali jste 12")
+// }else{
+//     alert("Nezadali jste 5")
+
+// }
+
+
+// DOVOLENA
+
+const destinace = prompt("Vyberte: Francie,Turecko,Česko")
+if(destinace=='Francie' || destinace=='francie'){
+    alert("Francie je skvěla volba")
+}else if(destinace=='Turecko' || destinace=='Turecko'){
+    alert("Turecko je skvěla volba")
+}else if(destinace=='Česko' || destinace=='česko'){
+    alert("Česko je skvěla volba")
+}else{
+    alert("Vyberte prosim y toho: Francie,Turecko,Česko")
+}
+
+//ZVIRATKA
+
+alert("Zadejte svá 3 oblíbene zvařatka")
+const animals1 = prompt("Zadejte své 1 oblíbene zvařatko")
+const animals2 = prompt("Zadejte své 2 oblíbene zvařatko")
+const animals3 = prompt("Zadejte své 3 oblíbene zvařatko")
+
+if (animals1,animals2,animals3=="Plamenak") {
+    alert("super")
+} else {
+    alert("dobry")
+    
+}
